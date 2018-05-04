58f4a27a8a69867f0c9486d03441519ac9a84ca0
Mason Ham <mason.ham@mass.gov>
2018-05-03 21:16:41 -0400

    Added the messages for commands

--- PhotoGroove.elm
+++ PhotoGroove.elm
@@ -4,6 +4,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Array exposing (Array)
+import Random 
 
 type alias Photo =
     { url : String }
@@ -11,17 +12,31 @@ type alias Photo =
 type alias Model = 
     {  photos : List Photo
     , selectedUrl : String
+    , chosenSize : ThumbnailSize
     }
 
-type alias Msg =
-    { operation : String
-    , data : String
-    }
+type Msg 
+    = SelectedByUrl String
+    | SurpriseMe
+    | SetSize ThumbnailSize
+    | SeletectByIndex Int
 
 type ThumbnailSize
     = Small
     | Medium
     | Large
+
+update : Msg -> Model -> ( Model, Cmd Msg)
+update msg model =
+    case msg of
+        SelectedByUrl url ->
+            ({ model | selectedUrl = url },Cmd.none)
+        SurpriseMe ->
+            ( model, Random.generate SeletectByIndex randomPhotoPicker )
+        SetSize size ->
+            ( { model | chosenSize = size}, Cmd.none)
+        SeletectByIndex index ->
+            ( { model | selectedUrl = getPhotoUrl index }, Cmd.none )
 
 initialModel : Model
 initialModel = 
@@ -32,6 +47,7 @@ initialModel =
         , { url = "3.jpeg"}
         ]
         , selectedUrl = "1.jpeg"
+        , chosenSize = Small
     }
 
 photoArray : Array Photo
@@ -42,31 +58,64 @@ urlPrefix : String
 urlPrefix = 
     "http://elm-in-action.com/"
 
-update : Msg -> Model -> Model
-update msg model =
-    case msg.operation of
-        "SELECT_PHOTO" ->
-            { model | selectedUrl = msg.data}
-        "SURPRISE_ME" ->
-            { model | selectedUrl = "2.jpeg"}
-        _ ->
-            model
+getPhotoUrl : Int -> String
+getPhotoUrl index =
+    case Array.get index photoArray of
+        Just photo ->
+            photo.url
+        Nothing ->
+            ""
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+        Medium ->
+            "medium"
+        Large ->
+            "large"
+
+sizeToClass : ThumbnailSize -> String
+sizeToClass size =
+    case size of
+        Small ->
+            "small"
+        Medium ->
+            "med"
+        Large ->
+            "large"
+
+randomPhotoPicker : Random.Generator Int
+randomPhotoPicker =
+    Random.int 0 ( Array.length photoArray - 1 )
 
 viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumbnail =
     img [ src (urlPrefix ++ thumbnail.url)
             , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
-            , onClick { operation = "SELECT_PHOTO", data = thumbnail.url} 
+            , onClick ( SelectedByUrl thumbnail.url )
             ]
             []
+
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input [ type_ "radio", name "size", onClick ( SetSize size ) ] []
+        , text ( sizeToString size )
+        ]
+
 view : Model -> Html Msg
 view model = 
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , button 
-            [ onClick { operation = "SURPRISE_ME", data = "" } ]
+            [ onClick SurpriseMe ]
             [ text "Surprise Me" ]
-        , div [ id "thumbnails" ] 
+        , h3 [] [ text "Thumbnail Size: " ]
+        , div [ id "choose-size" ]
+            [ viewSizeChooser Small, viewSizeChooser Medium, viewSizeChooser Large ]
+        , div [ id "thumbnails", class (sizeToClass model.chosenSize) ] 
             (List.map ( viewThumbnail model.selectedUrl ) model.photos)
         , img 
             [ class "large" 
@@ -74,10 +123,11 @@ view model =
             ]
             []
         ]
-
+main : Program Never Model Msg
 main = 
-    Html.beginnerProgram
-    {   model = initialModel
+    Html.program
+    {   init = ( initialModel, Cmd.none)
         , view = view
         , update = update
+        , subscriptions = (\model -> Sub.none)
     }
